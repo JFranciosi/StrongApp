9d244af010196e620fc7d8b14f526bad33e01629
JFranciosi <luigigigio.jf@gmail.com>
2025-12-11 10:03:48 +0100

    Create Add-exercise component

---
+++ src/app/components/add-exercise/add-exercise.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-exercise',
+  imports: [],
+  templateUrl: './add-exercise.html',
+  styleUrl: './add-exercise.css',
+})
+export class AddExercise {
+
+}
