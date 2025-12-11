bf470836fa90edf0003ecd56206103e34d2b0e3b
JFranciosi <luigigigio.jf@gmail.com>
2025-12-11 10:19:04 +0100

    Add LucideAngular Module to use custom icons

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,19 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { LucideAngularModule, Pencil, Plus, X } from 'lucide-angular';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),  
+    provideHttpClient(),
+        importProvidersFrom(
+      LucideAngularModule.pick({X,Pencil,Plus
+      })
+    ),
+  ],
 };
