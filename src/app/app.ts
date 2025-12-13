27207ff12b6a0a63bcb48b05bcb66e035ba66a20
JFranciosi <luigigigio.jf@gmail.com>
2025-12-13 16:51:24 +0100

    feat: Implemented side-menu and navbar component to improved accessibility from all device

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Footer } from './components/footer/footer';
+import { SideMenu } from './components/side-menu/side-menu';
+import { Navbar } from './components/navbar/navbar';
+import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Footer],
+  imports: [RouterOutlet, Footer, SideMenu, LucideAngularModule, Navbar],
   template: `
-    <router-outlet></router-outlet>
-    <app-footer></app-footer>
+    <app-navbar (menuClick)="isMenuOpen = true"></app-navbar>
+
+    <app-side-menu [isOpen]="isMenuOpen" (closeMenu)="isMenuOpen = false"></app-side-menu>
+
+    <div class="app-content">
+      <router-outlet></router-outlet>
+      <app-footer></app-footer>
+    </div>
   `,
   styleUrl: './app.css'
 })
 export class App {
-
+  isMenuOpen = false;
 }
---
+++ src/app/components/navbar/navbar.ts
@@ -0,0 +1,37 @@
+import { Component, EventEmitter, Output, HostBinding, HostListener } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { LucideAngularModule } from 'lucide-angular';
+
+@Component({
+    selector: 'app-navbar',
+    standalone: true,
+    imports: [CommonModule, LucideAngularModule, RouterModule],
+    templateUrl: './navbar.html',
+    styleUrl: './navbar.css'
+})
+export class Navbar {
+    @Output() menuClick = new EventEmitter<void>();
+    @HostBinding('class.hidden') isHidden = false;
+
+    private lastScrollTop = 0;
+
+    @HostListener('window:scroll')
+    onScroll() {
+        const currentScroll = window.scrollY || document.documentElement.scrollTop;
+
+        if (currentScroll > this.lastScrollTop && currentScroll > 64) {
+            // Scrolling down & past navbar height
+            this.isHidden = true;
+        } else {
+            // Scrolling up
+            this.isHidden = false;
+        }
+
+        this.lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+    }
+
+    toggleMenu() {
+        this.menuClick.emit();
+    }
+}
